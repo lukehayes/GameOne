da571cf84ba3bf003752dcdb9483a1c6a157d7fd
lukehayes <lukepurplehayes@gmail.com>
2025-07-11 10:52:07 +0100

    feat: add Color.YELLOW constant

--- F/Color.lua
+++ F/Color.lua
@@ -27,5 +27,6 @@ Color.GREY     = { r = 0.3, g = 0.3, b = 0.3, a = 1 }
 Color.RED      = { r = 1, g = 0, b = 0, a = 1 }
 Color.GREEN    = { r = 0, g = 1, b = 0, a = 1 }
 Color.BLUE     = { r = 0, g = 0, b = 1, a = 1 }
+Color.YELLOW   = { r = 1, g = 1, b = 0, a = 1 }
 
 return Color
