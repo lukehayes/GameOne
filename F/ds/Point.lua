2dda0df69b3fd9a0ec77392af5ad35c1a2daa8bd
lukehayes <lukepurplehayes@gmail.com>
2025-07-13 10:25:10 +0100

    feat: add Point:draw()

--- F/ds/Point.lua
+++ F/ds/Point.lua
@@ -9,7 +9,7 @@
 /*                                                                      */
 /*  Description:                                                        */
 /*                                                                      */
-/*  Point class                                                      */
+/*  Point class                                                         */
 /*  -------------------------------------------------------------       */
 /*  https://www.github.com/                                             */
 /*                                                                      */
@@ -17,15 +17,46 @@
 /************************************************************************/
 ]]
 
+local Color = require 'F.Color'
+
 local Point = {}
 Point.__index = Point
 
+---Create a new instance of Point.
+---@param x number The x position.
+---@param y number The y position.
+---@return table
 function Point:new(x,y)
   local obj = {
     x = x,
     y = y,
+    color = Color.WHITE
   }
-  return obj
+  return setmetatable(obj, Point)
+end
+
+---Render the point point to the screen
+---@param x number The x position.
+---@param y number The y position.
+---@return table
+function Point:draw()
+
+  love.graphics.setColor(
+    self.color.r,
+    self.color.g,
+    self.color.b,
+    self.color.a
+  )
+
+  love.graphics.rectangle(
+    'fill',
+      self.x,
+      self.y,
+      1,
+      1
+  )
+
+  love.graphics.setColor(1,1,1,1)
 end
 
 return Point
