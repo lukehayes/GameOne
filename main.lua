b2df2baea74b0ec8f57f4202d12ea14299d41c72
lukehayes <lukepurplehayes@gmail.com>
2025-07-16 22:13:30 +0100

    test: add area test code to main

--- main.lua
+++ main.lua
@@ -1,8 +1,17 @@
 local Game = require("F.Game")
 local Util = require("F.Util")
+local Col = require("F.col.Collision")
+local R   = require("F.gfx.Renderer")
+
+require 'F.Player'
+require 'F.Area'
 
 local Point = require("F.ds.Point")
 require("F.ds.Quadtree")
+
+local p  = PlayerCreate(300,300)
+local a1 = AreaCreate(100,100,100,120)
+local pa = AreaCreate(p.x,p.y,40,70)
 
 local points = {}
 
@@ -27,7 +36,6 @@ Game.events:emit("preload")
 
 function love.load()
   Game.events:emit("load")
-  love.graphics.getLineWidth(10)
 end
 
 function love.update(dt)
@@ -37,11 +45,26 @@ function love.update(dt)
 
   local mx, my = love.mouse.getPosition()
 
-  if love.keyboard.isDown("space") then
+  if love.mouse.isDown(1) then
     local p = Point:new(mx, my)
     QuadTreeInsert(qt, p)
     table.insert(points, p)
   end
+
+  PlayerInput(p)
+  
+  if Col.collides(p, a1) then
+
+    if love.keyboard.isDown('space') then
+      a1.x = love.math.random(100,500)
+      a1.y = love.math.random(100,500)
+      a1.w = love.math.random(10,500)
+      a1.h = love.math.random(10,500)
+    end
+  end
+  
+  pa.x = p.x - (pa.w / 2) + p.w / 2
+  pa.y = p.y - (pa.h / 2) + p.h / 2
 
   Game.events:emit("postupdate")
 end
@@ -49,11 +72,13 @@ end
 function love.draw()
   Game.events:emit("prerender")
 
-  QuadTreeDraw(qt)
-
   for _, point in pairs(points) do
     point:draw()
   end
+  
+  R.draw(p)
+  R.draw(pa)
+  R.draw(a1)
 
   Game.events:emit("postrender")
 end
