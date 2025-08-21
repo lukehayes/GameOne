1736f7b0fa24766ee99dcc4f907f4b6fb39dd75c
lukehayes <lukepurplehayes@gmail.com>
2025-08-21 09:24:54 +0100

    refactor: cleaned up main.lua

--- main.lua
+++ main.lua
@@ -1,36 +1,16 @@
 local Game = require("F.Game")
 local Util = require("F.Util")
-local Col = require("F.col.Collision")
 local R   = require("F.gfx.Renderer")
 
 require 'F.Player'
 require 'F.Area'
 
-local Point = require("F.ds.Point")
-require("F.ds.Quadtree")
-
 local p  = PlayerCreate(300,300)
 local a1 = AreaCreate(100,100,100,120)
 local pa = AreaCreate(p.x,p.y,40,70)
 
-local points = {}
-
--- for _ = 1, 1330 do
---   local rx = love.math.random(10, 800)
---   local ry = love.math.random(10, 600)
---
---   local p = Point:new(rx, ry)
---
---   table.insert(points, p)
--- end
-
--- local qt = nil
+-- Move to global class
 local scrW, scrH = love.graphics.getDimensions()
-local qt = QuadTreeCreate(0, 0, scrW, scrH)
-
--- for _, point in pairs(points) do
---   QuadTreeInsert(qt, point)
--- end
 
 Game.events:emit("preload")
 
@@ -41,50 +21,13 @@ end
 function love.update(dt)
   Game.events:emit("preupdate")
 
-  local scrW, scrH = love.graphics.getDimensions()
-
-  local mx, my = love.mouse.getPosition()
-
-  if love.mouse.isDown(1) then
-    local p = Point:new(mx, my)
-    QuadTreeInsert(qt, p)
-    table.insert(points, p)
-  end
-
-  if love.keyboard.isDown('x') then
-    print(QuadtreeNodeCount(qt))
-    points = {}
-    QuadtreeClear(qt)
-  end
-
   PlayerInput(p)
-
-  if Col.collides(p.area, a1) then
-
-    if love.keyboard.isDown('space') then
-      a1.x = love.math.random(100,500)
-      a1.y = love.math.random(100,500)
-      a1.w = love.math.random(10,500)
-      a1.h = love.math.random(10,500)
-    end
-  end
-
-  pa.x = p.x - (pa.w / 2) + p.w / 2
-  pa.y = p.y - (pa.h / 2) + p.h / 2
 
   Game.events:emit("postupdate")
 end
 
 function love.draw()
   Game.events:emit("prerender")
-
-  if love.keyboard.isDown('space') then
-    QuadTreeDraw(qt)
-  end
-
-  for _, point in pairs(points) do
-    point:draw()
-  end
 
   PlayerDraw(p,true)
 
